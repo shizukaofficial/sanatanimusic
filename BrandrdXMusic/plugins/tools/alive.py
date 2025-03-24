f185663fe3dc7c0dca22be18e93549b24153becc
ARYANSONI9026 <soniaryan2240021@gmail.com>
2025-03-24 22:39:09 +0530

    Update alive.py

--- BrandrdXMusic/plugins/tools/alive.py
+++ BrandrdXMusic/plugins/tools/alive.py
@@ -18,12 +18,12 @@ async def start(client: Client, message: Message):
                 text="☆ ᴀɴɪᴍᴀʟ 💗 ", url=f"https://t.me/smartness_to_hai"
             ),
             InlineKeyboardButton(
-                text="☆ ꜱᴜᴘᴘᴏʀᴛ 💗", url=f"https://t.me/hamari_duniyaa"
+                text="☆ ꜱᴜᴘᴘᴏʀᴛ 💗", url=f"https://t.me/knight_society"
             ),
         ],
                 [
             InlineKeyboardButton(
-                text="☆ ᴄʜᴀɴɴᴇʟ💗", url=f"https://t.me/Love_wavess"
+                text="☆ ᴄʜᴀɴɴᴇʟ💗", url=f"https://t.me/tabahi_tabahi"
             ),
                 ],
                 [
