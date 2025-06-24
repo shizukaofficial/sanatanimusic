a2562a86849dbbd768833e5c1e698d9301ddad0e
ARYANSONI9026 <soniaryan2240021@gmail.com>
2025-06-24 20:21:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -42,8 +42,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/About_Besties_Gautam")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Besties_Friends_Chat_Group")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Friends_Chatting_Group_Talking")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Besties_friends_chatting_groups")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -89,13 +89,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/6ss82g.jpg"
+    "START_IMG_URL", "https://i.ibb.co/SDB68PDg/Img2url-bot.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/6ss82g.jpg"
+    "PING_IMG_URL", "https://i.ibb.co/SDB68PDg/Img2url-bot.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/6ss82g.jpg"
+STATS_IMG_URL = "https://i.ibb.co/SDB68PDg/Img2url-bot.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/923c1faac33d8c70335dc.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6c66f8b192532fe758e82.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/ebc4dc6357be06e08a3ed.jpg"
